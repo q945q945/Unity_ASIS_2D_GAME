56a8fa3a8ee63f1df5eff9e14cacebc7333a55dd
q945q945 <71687772+q945q945@users.noreply.github.com>
2020-11-03 15:56:47 +0800

    1103_2d_game

--- 2D_GAME/Assets/Scripts/Player.cs
+++ 2D_GAME/Assets/Scripts/Player.cs
@@ -2,10 +2,11 @@
 
 public class Player : MonoBehaviour
 {
-
+    public GameObject final;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         print("碰到傳送門");
+        final.SetActive(true);
     }
 
 }
