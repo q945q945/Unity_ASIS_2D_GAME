93514733d451e33845ba275495395e4b90559e1e
q945q945 <71687772+q945q945@users.noreply.github.com>
2020-11-10 16:02:45 +0800

    1110_2D_METERM

--- ASIA_2D_Midterm_107051196/Assets/scripts/Player.cs
+++ ASIA_2D_Midterm_107051196/Assets/scripts/Player.cs
@@ -1,13 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public GameObject Final;
+    public Text TextCount;
+    public int count;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("碰到門");
-        Final.SetActive(true);
-    }
+        if (collision.name == "門")
 
+        {
+            Final.SetActive(true);
+        }
+        if (collision.tag == "西瓜")
+        {
+            Destroy(collision.gameObject);
+            count++;
+            TextCount.text = "scare:" + count;
+        }
+    }
+       
+    
+    
+	
 }
